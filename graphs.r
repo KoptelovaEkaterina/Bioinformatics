5731b239b17d0f9d22fe79f3e8950168557a9cb0
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-06-30 02:37:06 +0300

    Update graphs.r

--- graphs.r
+++ graphs.r
@@ -1,2 +1,6 @@
 gg_mel <- ggplot(USmelanoma, aes(x=mortality)) +
 + geom_density(fill="darkorange", colour="black") + theme_dark()
+
+gg_Leinhardt <- ggplot(Leinhardt, aes(x=income)) + 
++geom_histogram(binwidth = 250, aes(fill=region)) +
++ facet_wrap(~region,nrow=4) + theme_light()
