67f043f70de712cb9b9c79279cf1fb0fd1cd3d51
Ekaterina Koptelova <76552692+KoptelovaEkaterina@users.noreply.github.com>
2022-06-30 23:01:15 +0300

    Update vectors.r

--- vectors.r
+++ vectors.r
@@ -1,7 +1,8 @@
 
 # The built-in dataframe stackloss describes the operating parameters of a nitric acid plant.
 # Use the logical vector air_fltr to select values of nitric acid concentration for which the cooling air flow is less than 60. 
-# The acid_subset variable stores the sampled values themselves, and the acid_concs variable stores the frequency table of unique acid concentration values for these sampled data.
+# The acid_subset variable stores the sampled values themselves, 
+# and the acid_concs variable stores the frequency table of unique acid concentration values for these sampled data.
 # https://stepik.org/lesson/121889/step/14
 
 # logical vector
@@ -37,3 +38,26 @@ vec3 <- c(0, 0, 1, 1, 0, 1)
  dfr <- data.frame(id = vec1,stringsAsFactors = FALSE)
  dfr$response <- factor(x = vec3, levels = c(0, 1) , labels = c('No','Yes'))
 dfr$medium <- vec2
+
+
+
+# It is written in the abstract of some course that it will take an average of 2 hours per week to do homework. 
+# Surveying 50 randomly selected students, it turned out that they spent an average of 2.4 hours on preparation (standard deviation 1.5 hours). 
+# At the same time, the time spent on homework is normally distributed. The value of t-statistics and the level of significance were calculated.
+# https://stepik.org/lesson/122051/step/12
+
+# Sample volume
+N <- 50
+
+# Time spent by students in preparation
+observed_time <- 2.4 # average
+observed_s <- 1.5 # standard deviation
+
+# Time stated in course outline
+expected_time <- 2
+
+# value of t-statistic
+t_val <- ( observed_time - expected_time)/(observed_s/sqrt(N))
+
+# Level of significance
+p_val <- 2*(1 - pt(t_val, df = N-1))
